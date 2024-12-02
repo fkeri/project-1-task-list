6c10d88c2cd218e9bac1e70598c5201a6b413eac
Filip Keri <filipkeri@Filips-MacBook-Air.local>
2024-12-02 17:00:51 +0100

    Refactor - Mass Assignment with TaskRequest (1.19)

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,7 @@
 
 use App\Http\Requests\TaskRequest;
 use App\Models\Task;
-use Illuminate\Http\Request;
-use Illuminate\Http\Response as HttpResponse;
 use Illuminate\Support\Facades\Route;
-
 
 Route::get('/', function() {
     return redirect()->route('tasks.index');
@@ -32,25 +29,14 @@ Route::get('/tasks/{task}/edit', function(Task $task) {
 })->name('tasks.edit');
 
 Route::post('/tasks', function(TaskRequest $request) {
-    $data = $request->validated();
-    $task = new Task;
-    $task->title = $data['title'];
-    $task->description = $data['description'];
-    $task->long_description = $data['long_description'];
-
-    $task->save();
+    $task = Task::create($request->validated());
 
     return redirect()->route('tasks.show', ['task' => $task->id])
         ->with('success', 'Task created successfully!');
 })->name('tasks.store');
 
 Route::put('/tasks/{task}', function(Task $task, TaskRequest $request) {
-    $data = $request->validated();
-    $task->title = $data['title'];
-    $task->description = $data['description'];
-    $task->long_description = $data['long_description'];
-
-    $task->save();
+    $task->update($request->validated());
 
     return redirect()->route('tasks.show', ['task' => $task->id])
         ->with('success', 'Task updated successfully!');
