331b9b5bec78602b75ecb3a5a324be0d057befd6
Filip Keri <filipkeri@Filips-MacBook-Air.local>
2024-12-03 12:20:42 +0100

    Deleting Data (1.20)

--- routes/web.php
+++ routes/web.php
@@ -41,3 +41,10 @@ Route::put('/tasks/{task}', function(Task $task, TaskRequest $request) {
     return redirect()->route('tasks.show', ['task' => $task->id])
         ->with('success', 'Task updated successfully!');
 })->name('tasks.update');
+
+Route::delete('/taks/{task}', function (Task $task) {
+    $task->delete();
+
+    return redirect()->route('tasks.index')
+        ->with('success', 'Task deleted successfully!');
+})->name('tasks.destroy');
